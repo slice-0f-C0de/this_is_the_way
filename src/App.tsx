494762c0b8c9dc93f338b90502b0c58694c53114
slice_of_code <slice_of_code@mail.ru>
2023-01-20 20:06:00 +0400

    first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
-import {Navbar} from "./Navbar";
-import {Profile} from "./Profile";
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
